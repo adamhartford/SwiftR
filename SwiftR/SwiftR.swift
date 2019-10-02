f56af3a740c0d5ee9e7e07ffaea8093f94ddb920
Adolph <iceboxi@Adolph-MBPR.local>
2019-10-02 13:54:55 +0800

    add signalr v2.4.1 and default setup to v2.4.1

--- SwiftR/SwiftR.swift
+++ SwiftR/SwiftR.swift
@@ -79,7 +79,7 @@ open class SignalR: NSObject, SwiftRWebDelegate {
     var internalID: String!
     var ready = false
     
-    public var signalRVersion: SignalRVersion = .v2_4_0
+    public var signalRVersion: SignalRVersion = .v2_4_1
     public var useWKWebView = false
     public var transport: Transport = .auto
     /// load Web resource from the provided url, which will be used as Origin HTTP header
@@ -550,6 +550,7 @@ open class Hub: NSObject {
 }
 
 public enum SignalRVersion : CustomStringConvertible {
+    case v2_4_1
     case v2_4_0
     case v2_2_2
     case v2_2_1
@@ -564,6 +565,7 @@ public enum SignalRVersion : CustomStringConvertible {
     
     public var description: String {
         switch self {
+            case .v2_4_1: return "2.4.1"
             case .v2_4_0: return "2.4.0"
             case .v2_2_2: return "2.2.2"
             case .v2_2_1: return "2.2.1"
